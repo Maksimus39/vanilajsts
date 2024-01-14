cbfbad7b8ce1335e287fca3603fb8e87628b0a1e
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-01-14 16:46:18 +0300

    object

---
+++ src/02/02.ts
@@ -0,0 +1,56 @@
+type AddressType={
+    streetTitle:string
+    city:LocalCityType
+}
+
+type LocalCityType={
+    title:string
+    countryTitle:string
+}
+
+type TechType={
+    id:number
+    title:string
+}
+
+type StudentType = {
+    id: number
+    name: string
+    age: number
+    isActive: boolean
+    address:AddressType
+    technologies:Array<TechType>
+}
+
+
+
+
+
+const student: StudentType = {
+    id: 1,
+    name: "Max",
+    age: 40,
+    isActive: true,
+    address: {
+        streetTitle: "People's construction site",
+        city: {
+            title: "Lipetsk",
+            countryTitle: "Russia"
+        }
+    },
+    technologies: [
+        {
+            id: 1,
+            title: "HTML",
+        }, {
+            id: 2,
+            title: "CSS",
+        }, {
+            id: 3,
+            title: "React",
+        }
+    ]
+}
+
+console.log(student.address.city.countryTitle)
+console.log(student.technologies[2].title)
